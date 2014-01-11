95f333b0a46b1b561a511ec0c2c8926de8969c42
Nceptioj <fraseyboy@gmail.com>
2014-01-11 16:04:18 +1300

    Update self_updating_rss.py

--- self_updating_rss.py
+++ self_updating_rss.py
@@ -4,8 +4,8 @@
 import updater
 
 # Key, account, categories...
-THUNDERMAPS_API_KEY = "03a01ba4d1d3c60feec7fe7a4cc832b6"
-THUNDERMAPS_ACCOUNT_ID = "gdfgsdfg"
+THUNDERMAPS_API_KEY = ""
+THUNDERMAPS_ACCOUNT_ID = ""
 THUNDERMAPS_CATEGORY_ID = {"ISS Sighting": 11870, "CYGNUS Sighting": 11871}
 RSS_FEED_URL = 'http://spotthestation.nasa.gov/sightings/xml_files/New_Zealand_None_Wellington.xml'
 
