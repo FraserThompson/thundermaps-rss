d9a0c618d5a685979b464449eefc9e19f69b9ed5
Nceptioj <fraserthompson0@gmail.com>
2014-01-21 15:05:02 +1300

    updated slightly

--- self_updating_rss.py
+++ self_updating_rss.py
@@ -4,7 +4,7 @@
 import updater
 
 # Key, account, categories...
-THUNDERMAPS_API_KEY = ""
+THUNDERMAPS_API_KEY = "03a01ba4d1d3c60feec7fe7a4cc832b6"
 THUNDERMAPS_ACCOUNT_ID = ""
 RSS_FEED_URL = 'http://spotthestation.nasa.gov/sightings/xml_files/New_Zealand_None_Wellington.xml'
 
